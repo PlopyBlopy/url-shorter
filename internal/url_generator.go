9503cbce2beb505dbebd59ed1a98a56ec0187ed7
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-03-11 17:03:14 +0300

    feat(url_generator.go): создает короткие ссылки Описание: - Generator - структура что хранит counter и rep. counter загружается из базы данных Функции: - NewGenerator - конструктор - GenerateShortUrl - создает короткую ссылку

---
+++ internal/url_generator.go
@@ -0,0 +1,64 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"sync/atomic"
+)
+
+type Repository interface {
+	GetCounter(ctx context.Context) (uint64, error)
+}
+
+type Generator struct {
+	counter uint64
+	rep     Repository
+}
+
+func NewGenerator(rep Repository, ctx context.Context) (*Generator, error) {
+	counter, err := rep.GetCounter(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create Generator: %w", err)
+	}
+
+	return &Generator{
+		counter: counter,
+		rep:     rep,
+	}, nil
+}
+
+func (g *Generator) GenerateShortUrl() string {
+	buf := []byte{'h', 't', 't', 'p', 's', ':', '/', '/', 'c', 'l', 'i', 'c', 'k', '.', 'r', 'u', '/', ' ', ' ', ' ', ' ', ' ', ' '}
+
+	n := atomic.AddUint64(&g.counter, 1) - 1 // starts from 0 if first, or couter num
+
+	if n == math.MaxUint64 {
+		panic("The indicator value limit has been reached")
+	}
+
+	const (
+		base26 = 26
+		base9  = 9
+	)
+
+	buf[len(buf)-6] = byte('A' + n%base26)
+	n /= base26
+
+	buf[len(buf)-5] = byte('A' + n%base26)
+	n /= base26
+
+	buf[len(buf)-4] = byte('1' + n%base9)
+	n /= base9
+
+	buf[len(buf)-3] = byte('a' + n%base26)
+	n /= base26
+
+	buf[len(buf)-2] = byte('A' + n%base26)
+	n /= base26
+
+	buf[len(buf)-1] = byte('A' + n%base26)
+	n /= base26
+
+	return string(buf)
+}
