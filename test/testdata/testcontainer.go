d62c6a7d65905ebd8ce0804d44836bf91aef172e
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-03-11 14:10:06 +0300

    chore(tests): определены данные для тестов Описание: - Новые пакеты: testcontainers-go, testify, testify.assert - testcontainer.go - нужен для создания testcontainer и для Rollback snapshot что создается при старте БД - testdata.go - тестовые данне - test_init.sql - таблицы для тестовой БД = оригинальной БД

---
+++ test/testdata/testcontainer.go
@@ -0,0 +1,107 @@
+package testdata
+
+import (
+	"context"
+	"path/filepath"
+	"runtime"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+// Creates postgres container
+func NewPostgresTestcontainer(ctx context.Context) (*postgres.PostgresContainer, error) {
+	// Нужно для инициализации скрипта
+	// Получаем путь к текущему файлу и его дериктории
+	_, filename, _, _ := runtime.Caller(0)
+	currentDir := filepath.Dir(filename)
+
+	pgContainer, err := postgres.Run(ctx, "postgres:18-alpine",
+		postgres.WithInitScripts(filepath.Join(currentDir, "test_init.sql")),
+		postgres.WithDatabase("test-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return pgContainer, nil
+}
+
+// Stores the postgres container, manages snapshot and connections to isolate tests that require it.
+type TestSuite struct {
+	PgContainer  *postgres.PostgresContainer
+	Db           *pgxpool.Pool
+	snapshotName string
+}
+
+// Creates a TestSuite.
+func NewTestSuite(pgContainer *postgres.PostgresContainer, ctx context.Context) (*TestSuite, error) {
+	snapshotName := "base-snapshot"
+	err := pgContainer.Snapshot(ctx, postgres.WithSnapshotName(snapshotName))
+	if err != nil {
+		return nil, err
+	}
+
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = 4
+	config.MinConns = 1
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TestSuite{
+		PgContainer:  pgContainer,
+		Db:           pool,
+		snapshotName: snapshotName,
+	}, nil
+}
+
+// Completes active connections, applies base snapshot, restoring the database to its original form.
+func (s *TestSuite) SetupTest(ctx context.Context) error {
+	if s.Db != nil {
+		s.Db.Close()
+	}
+
+	err := s.PgContainer.Restore(ctx, postgres.WithSnapshotName(s.snapshotName))
+	if err != nil {
+		return err
+	}
+
+	connStr, err := s.PgContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return err
+	}
+
+	config, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return err
+	}
+
+	config.MaxConns = 1
+	config.MinConns = 1
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	s.Db = pool
+
+	return nil
+}
---
+++ test/testdata/testdata.go
@@ -0,0 +1,46 @@
+package testdata
+
+import (
+	"context"
+	"time"
+
+	"github.com/PlopyBlopy/url-shorter/internal"
+	"github.com/PlopyBlopy/url-shorter/internal/domain"
+)
+
+type testGenerator struct {
+	delta uint64
+}
+
+func (g *testGenerator) GetCounter(ctx context.Context) (uint64, error) {
+	return g.delta, nil
+}
+
+// Delta is base counter value generate from internal.generator. In app gets from DB. In testdata from origUrlDelta and shortUrlDelta. CratedAt increment on 1 hour.
+func GetUrls(count int, origUrlDelta, shortUrlDelta uint64) ([]domain.Url, error) {
+	out := make([]domain.Url, count)
+	g1 := &testGenerator{delta: origUrlDelta}
+	g2 := &testGenerator{delta: shortUrlDelta}
+
+	ctx := context.Background()
+
+	origGen, err := internal.NewGenerator(g1, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	shortGen, err := internal.NewGenerator(g2, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < count; i++ {
+		out[i] = domain.Url{
+			OrigUrl:   origGen.GenerateShortUrl(),
+			ShortUrl:  shortGen.GenerateShortUrl(),
+			CreatedAt: time.Now().Add(time.Hour).UTC().Truncate(time.Microsecond),
+		}
+	}
+
+	return out, err
+}
