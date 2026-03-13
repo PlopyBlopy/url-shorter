9173a9e1b598fc7631f587c77b73ad8ca656d41c
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-03-13 16:55:47 +0300

    feat(domain\errors.go): добавлена типизация ошибок, что бы не зависить от внешних пакетов и определенных в них ошибок ## Описание Была зависимость от пакета pgx и его ошибок в частности от pgx.ErrNoRows

---
+++ internal/domain/errors.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	CounterNoAdded  = "counter have not been added"
+	CounterNotFound = "no counter found"
+
+	URLSNoAdded      = "urls have not been added"
+	URLSNotFound     = "no urls found"
+	URLAlreadyExists = "the URL already exists"
+)
+
+var (
+	ErrCounterNoAdded  = errors.New(CounterNoAdded)
+	ErrCounterNotFound = errors.New(CounterNotFound)
+
+	ErrURLSNoAdded      = errors.New(URLSNoAdded)
+	ErrURLSNotFound     = errors.New(URLSNotFound)
+	ErrURLAlreadyExists = errors.New(URLAlreadyExists)
+)
+
+func ErrCounterNoAddedMsg(code, msg string) error {
+	return fmt.Errorf("%s: code=%s, message=%s", ErrCounterNoAdded.Error(), code, msg)
+}
