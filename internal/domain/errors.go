53d230d2b69cec63360d7f36450db8a51a7bb02a
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-03-23 13:48:42 +0300

    feat(errors.go): добавлена новая ошибка ErrEmptyData

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -6,21 +6,21 @@ import (
 )
 
 const (
-	CounterNoAdded  = "counter have not been added"
-	CounterNotFound = "no counter found"
-
+	CounterNoAdded   = "counter have not been added"
+	CounterNotFound  = "no counter found"
 	URLSNoAdded      = "urls have not been added"
 	URLSNotFound     = "no urls found"
 	URLAlreadyExists = "the URL already exists"
+	EmptyData        = "the data is empty"
 )
 
 var (
-	ErrCounterNoAdded  = errors.New(CounterNoAdded)
-	ErrCounterNotFound = errors.New(CounterNotFound)
-
+	ErrCounterNoAdded   = errors.New(CounterNoAdded)
+	ErrCounterNotFound  = errors.New(CounterNotFound)
 	ErrURLSNoAdded      = errors.New(URLSNoAdded)
 	ErrURLSNotFound     = errors.New(URLSNotFound)
 	ErrURLAlreadyExists = errors.New(URLAlreadyExists)
+	ErrEmptyData        = errors.New(EmptyData)
 )
 
 func ErrCounterNoAddedMsg(code, msg string) error {
