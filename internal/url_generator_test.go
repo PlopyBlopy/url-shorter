3a45ae13eea5b0cd2727153da59db29dc3f10f56
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-03-24 21:13:17 +0300

    test(url_generator_test.go): подтест на не повторяющиеся ссылки вынесен в отдельный тест

--- internal/url_generator_test.go
+++ internal/url_generator_test.go
@@ -24,42 +24,6 @@ func TestGenerate(t *testing.T) {
 		assert.Len(url, len("https://click.ru/XXXXXX"))
 	})
 
-	t.Run("no repetitions up to the maximum unique value", func(t *testing.T) {
-		// Arrange
-		require := require.New(t)
-		assert := assert.New(t)
-
-		g, err := NewGenerator()
-		require.NoError(err)
-
-		var max uint64 = maxUnique
-
-		part := uint64(6)
-
-		m := make(map[string]int, max)
-		repetitionCount := 0
-		var val uint64
-
-		// Act
-		for val = 0; val < max; val++ {
-			url := g.GenerateShortUrl(val)
-			m[url] = m[url] + 1
-
-			if m[url] > 1 {
-				repetitionCount++
-				fmt.Printf("key:%s, val:%d", url, m[url])
-			}
-
-			if part > 1 && val == max/part {
-				fmt.Printf("part:6/%d\n", part)
-				part--
-			}
-		}
-
-		// Assert
-		assert.Equal(0, repetitionCount)
-	})
-
 	t.Run("the maximum unique value limit has been reached", func(t *testing.T) {
 		// Arrange
 		require := require.New(t)
@@ -75,4 +39,42 @@ func TestGenerate(t *testing.T) {
 			_ = g.GenerateShortUrl(max)
 		})
 	})
+}
+
+// The test may end earlier due to the end of the timeout for the test.
+// It needs to run in debug or with an extended timeout.
+func TestNoRepetitionToMaxUniqueValue(t *testing.T) {
+	// Arrange
+	require := require.New(t)
+	assert := assert.New(t)
+
+	g, err := NewGenerator()
+	require.NoError(err)
+
+	var max uint64 = maxUnique
+
+	part := uint64(6)
+
+	m := make(map[string]int, max)
+	repetitionCount := 0
+	var val uint64
+
+	// Act
+	for val = 0; val < max; val++ {
+		url := g.GenerateShortUrl(val)
+		m[url] = m[url] + 1
+
+		if m[url] > 1 {
+			repetitionCount++
+			fmt.Printf("key:%s, val:%d", url, m[url])
+		}
+
+		if part > 0 && val == max/part {
+			fmt.Printf("part:6/%d\n", part)
+			part--
+		}
+	}
+
+	// Assert
+	assert.Equal(0, repetitionCount)
 }
